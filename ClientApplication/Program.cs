f2341f4499e3e0721a766566b0cb6d9a815d185a
97gabrielS <47827236+97gabrielS@users.noreply.github.com>
2020-04-30 04:39:21 +0100

    First version; all working

---
+++ ClientApplication/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using SubLangEncoder;
+
+namespace ClientApplication
+{
+    public class Program
+    {
+        private const string Lang = ".pt";
+
+        public static void Main()
+        {
+            var path = GetPath();
+            var encoder = new SubEncoder(path, Lang);
+            encoder.Encode();
+            Console.WriteLine($"All subtitle files within the path were coded to {Lang} language");
+        }
+
+        private static string GetPath()
+        {
+            string path;
+            do
+            {
+                Console.Write("Path: ");
+                path = Console.ReadLine();
+                if (!Directory.Exists(path))
+                {
+                    Console.WriteLine($"Error: Path \"{path}\" doesn't exist!\n");
+                }
+                else
+                {
+                    break;
+                }
+            } while (true);
+
+            return path;
+        }
+    }
+}
---
+++ SubLangEncoder/SubEncoder.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+
+// ReSharper disable StringIndexOfIsCultureSpecific.1
+
+namespace SubLangEncoder
+{
+    public class SubEncoder
+    {
+        private readonly string _path;
+        private readonly string _langCode;
+
+        public SubEncoder(string path, string langCode)
+        {
+            _path = path;
+            _langCode = langCode;
+        }
+
+        public void Encode()
+        {
+            var rootDirectory = new DirectoryInfo(_path);
+            var subtitleFiles = rootDirectory.EnumerateFiles("*.srt", SearchOption.AllDirectories);
+            foreach (var subFile in subtitleFiles)
+            {
+                var subFileName = subFile.Name;
+                if (!subFileName.Contains(_langCode))
+                {
+                    var newSubPath = GetNewSubPathWithLangCodeInserted(subFile.DirectoryName, subFile.Name);
+                    subFile.MoveTo(newSubPath);
+                }
+            }
+        }
+
+        private string GetNewSubPathWithLangCodeInserted(string subDirectoryPath, string subName)
+        {
+            var startIdx = subName.IndexOf(".srt");
+            var newSubFileName =
+                subName.Insert(startIdx, _langCode); // inserts the language code into the subtitle file's name
+            return $"{subDirectoryPath}\\{newSubFileName}";
+        }
+    }
+}
